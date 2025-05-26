2004059df669c45bb7f8871c29375a76eaf112fd
shamim <amshamim99@gmail.com>
2025-05-26 22:06:46 +0600

    create star rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Accordion from './components/Accordion'
 import RandomColor from './components/random-color/RandomColor'
+import RattingStar from './components/ratting-star/RattingStar'
 
 const App = () => {
   return (
     <div>
         <Accordion/>
         <RandomColor/>
+        <RattingStar/>
     </div>
     
   )
